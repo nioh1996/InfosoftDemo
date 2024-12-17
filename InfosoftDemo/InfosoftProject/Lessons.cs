08460214b52074f3eef9a63df267e7e2d68a6d4f
nioh1996 <98360731+nioh1996@users.noreply.github.com>
2024-12-17 16:50:30 +0800

    Add files via upload

---
+++ InfosoftDemo/InfosoftProject/Lessons.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfosoftDemo
+
+{
+    class Lessons
+    {
+
+        //Lesson 1: Introduction to C#
+        public void Helloworld_l1()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //Lesson 2: Data Variables and Data Types
+        public void VariablesandDatatype_l2()
+        {
+            String fname, lname, gender;
+            int age;
+            float gpa;
+
+            fname = "Migs";
+            lname = "Jimenez";
+            gender = "Male";
+            age = 21;
+            gpa = 4.0f;
+
+            Console.WriteLine("Name : " + (fname + lname));
+            Console.WriteLine("Gender : " + gender);
+            Console.WriteLine("Age : " + age);
+            Console.WriteLine("Gpa : " + gpa);
+        }
+
+        //Lesson 3: Basic Input and Output
+        public void InputOutput_l3()
+        {
+
+            Console.WriteLine("Enter your name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter your Location: ");
+            string Location = Console.ReadLine();
+            Console.WriteLine("Enter your Company: ");
+            string Company = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Hi My Name is " + Name);
+            Console.WriteLine("I am from " + Location);
+            Console.WriteLine("I am currently working at " + Company);
+
+        }
+
+        //Lesson 4: Operators and Expressions
+        public void OperatorsandExpressions_l4()
+        {
+            float average, grade1, grade2, grade3, grade4;
+
+            Console.WriteLine("Enter your Grade in English: ");
+            grade1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter your Grade in Math: ");
+            grade2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter your Grade in Science: ");
+            grade3 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter your Grade in Computer: ");
+            grade4 = Convert.ToSingle(Console.ReadLine());
+
+            average = (grade1 + grade2 + grade3 + grade4) / 4;
+            Console.WriteLine("Your Average is : " + average);
+        }
+
+        //Lesson 5: Conditional Statements
+        public void ConditionalStatements_l5()
+        {
+            float average, grade1, grade2, grade3, grade4;
+
+            Console.WriteLine("Enter your Grade in English: ");
+            grade1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter your Grade in Math: ");
+            grade2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter your Grade in Science: ");
+            grade3 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Enter your Grade in Computer: ");
+            grade4 = Convert.ToSingle(Console.ReadLine());
+
+            average = (grade1 + grade2 + grade3 + grade4) / 4;
+            Console.WriteLine("Your Average is : " + average);
+
+            Console.WriteLine();
+
+            if (average > 100)
+            {
+                Console.WriteLine("Invalid Grade");
+            }
+            else if (average >= 98)
+            {
+                Console.WriteLine("With Highest Honor");
+            }
+            else if (average >= 95)
+            {
+                Console.WriteLine("With Highest Honor");
+            }
+            else if (average >= 90)
+            {
+                Console.WriteLine("With High Honor");
+            }
+            else if (average >= 75)
+            {
+                Console.WriteLine("With Highest Honor");
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+            }
+        }
+
+        //Lesson 6: Switch Statements
+        public void SwitchStatements_l6()
+        {
+            int month, date, year;
+            String monthword;
+
+            Console.WriteLine("Enter your birthmonth (number): ");
+            month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your birthdate (number): ");
+            date = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your birthyear: ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            switch (month)
+            {
+
+                case 1:
+                    monthword = "January";
+                    break;
+                case 2:
+                    monthword = "February";
+                    break;
+                case 3:
+                    monthword = "March";
+                    break;
+                case 4:
+                    monthword = "April";
+                    break;
+                case 5:
+                    monthword = "May";
+                    break;
+                case 6:
+                    monthword = "June";
+                    break;
+                case 7:
+                    monthword = "July";
+                    break;
+                case 8:
+                    monthword = "August";
+                    break;
+                case 9:
+                    monthword = "September";
+                    break;
+                case 10:
+                    monthword = "October";
+                    break;
+                case 11:
+                    monthword = "November";
+                    break;
+                case 12:
+                    monthword = "December";
+                    break;
+                default:
+                    monthword = "Invalid Month";
+                    break;
+            }
+
+            if (month <= 12 && date <= 31)
+            {
+                Console.WriteLine(monthword + " " + date + ", " + year);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+
+        //Lesson 7: Loops : While loop
+        public void WhileLoop_l7()
+        {
+            int health = 5;
+            string CorrectAnswer = "Infosoft";
+            string answer;
+
+            while (health > 0)
+            {
+                Console.WriteLine("What company am I working with right now? : ");
+                answer = Console.ReadLine();
+                if (answer == CorrectAnswer)
+                {
+                    Console.WriteLine("Your answer is Correct!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Your answer is Wrong! you have " + (health - 1) + " Chances!.");
+                    health--;
+                }
+
+                if (health == 0)
+                {
+                    Console.WriteLine("You Lost!");
+                }
+            }
+
+        }
+
+        //Lesson 7: Loops : For loop (username same as password)
+        public void Forloop_l7()
+        {
+            String username, password;
+            bool isFound = false;
+
+            string[] uName = {
+                "KurtDavid",
+                "KarlPaclar",
+                "MigsJimenez"
+            };
+            string[] pWord = {
+                "KurtDavid",
+                "KarlPaclar",
+                "MigsJimenez"
+            };
+
+            Console.WriteLine("Enter Username: ");
+            username = Console.ReadLine();
+            Console.WriteLine("Enter Password: ");
+            password = Console.ReadLine();
+
+
+            for (int i = 0; i < uName.Length; i++)
+            {
+                if (uName[i].Equals(username) && pWord[i].Equals(password))
+                {
+                    Console.WriteLine("Welcome " + username[i]);
+                    isFound = true;
+                    break;
+                }
+              
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine("Account Not Found!");
+            }
+
+        }
+
+        //Lesson 8: Arrays
+        public void Array_l8()
+        {
+            int input;
+            string[] Email = {"kurtdade24@gmail.com",
+                "migueljimenez@gmail.com",
+                "karlpaclar@gmail.com"
+            };
+            string[] Username = {"Kurt",
+                "Miguel",
+                "Karl" 
+            };
+            string[] Password = {
+                "kurt","miguel","karlpaclar"
+            };
+
+            Console.WriteLine("Enter Index: ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Index: "+ input);
+            Console.WriteLine("Email: " + Email[input]);
+            Console.WriteLine("Username: " + Username[input]);
+            Console.WriteLine("Password: " + Password[input]);
+        }
+
+        //Lesson 9: Functions
+
+        public int Functions_l9(int[] numbers)
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++) {
+
+                sum += numbers[i];  
+            }
+
+            return sum;
+
+        }
+
+    }
+}
---
+++ InfosoftDemo/InfosoftProject/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace InfosoftDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lessons lessons = new Lessons();
+            
+
+            //Lesson 1: Introduction to C#
+            Console.WriteLine("Lesson 1: Introduction to C#");
+            lessons.Helloworld_l1();
+            
+            Console.WriteLine();
+
+            //Lesson 2: Data Variables and Data Types
+            Console.WriteLine("Lesson 2: Data Variables and Data Types");
+            lessons.VariablesandDatatype_l2();
+
+            Console.WriteLine();
+
+            //Lesson 3: Basic Input and Output
+            Console.WriteLine("Lesson 3: Basic Input and Output");
+            lessons.InputOutput_l3();
+
+            Console.WriteLine();
+            
+            //Lesson 4: Operators and Expressions
+            Console.WriteLine("Lesson 4: Operators and Expressions");
+            lessons.OperatorsandExpressions_l4();
+
+            Console.WriteLine();
+            
+            //Lesson 5: Conditional Statements
+            Console.WriteLine("Lesson 5: Conditional Statements");
+            lessons.ConditionalStatements_l5();
+
+            Console.WriteLine();
+
+            //Lesson 6: Switch Statements
+            Console.WriteLine("Lesson 6: Switch Statements");
+            lessons.SwitchStatements_l6();
+
+            Console.WriteLine();
+
+            //Lesson 7: Loops : While loop
+            Console.WriteLine("Lesson 7: Loops : While loop");
+            lessons.WhileLoop_l7();
+
+            Console.WriteLine();
+
+            //Lesson 7: Loops : For loop
+            Console.WriteLine("Lesson 7: Loops : For loop");
+            lessons.Forloop_l7();
+
+            Console.WriteLine();
+
+            //Lesson 8: Arrays
+            Console.WriteLine("Lesson 8: Arrays");
+            lessons.Array_l8();
+
+            Console.WriteLine();
+
+            //Lesson 9: Functions
+            Console.WriteLine("Lesson 9: Functions");
+            int result = lessons.Functions_l9(new int[]{5,6,4,3,2,6});
+            Console.WriteLine("The sum is: "+ result);
+        }
+    }
+}
